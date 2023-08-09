72b78eda2733feabab88c95296a7b61953f71c20
heiyan <heiyan2020@gmail.com>
2023-08-09 18:04:42 +0800

    chore: define cache key struct

--- src/sql/das/ob_das_cache.h
+++ src/sql/das/ob_das_cache.h
@@ -14,13 +14,60 @@
 #define OBDEV_SRC_SQL_DAS_OB_DAS_CACHE_H_
 
 #include "share/cache/ob_kv_storecache.h"
+#include "common/rowkey/ob_rowkey.h"
 
 namespace oceanbase
 {
 namespace sql
 {
 
-class ObDASCache : public common::ObKVCache<ObFuseRowCacheKey, ObFuseRowCacheValue> {
+class ObDASCacheKey : public common::ObIKVCacheKey
+{
+public:
+	ObDASCacheKey();
+	ObDASCacheKey(
+      const uint64_t tenant_id,
+      const ObTabletID &tablet_id,
+      const ObRowKey &rowkey);
+  virtual ~ObDASCacheKey() = default;
+  virtual int equal(const ObIKVCacheKey &other, bool &equal) const override;
+  virtual int hash(uint64_t &hash_value) const override;
+  virtual uint64_t get_tenant_id() const override;
+  virtual int64_t size() const override;
+  virtual int deep_copy(char *buf, const int64_t buf_len, ObIKVCacheKey *&key) const override;
+  bool is_valid() const;
+  TO_STRING_KV(K_(tenant_id), K_(tablet_id), K_(rowkey_size), K_(rowkey));
+private:
+  uint64_t tenant_id_;
+  ObTabletID tablet_id_;
+  int64_t rowkey_size_;
+  ObRowKey rowkey_;
+  DISALLOW_COPY_AND_ASSIGN(ObDASCacheKey);
+};
+
+class ObDASCacheValue : public common::ObIKVCacheValue
+{
+//public:
+//	ObDASCacheValue();
+//  virtual ~ObDASCacheValue() = default;
+//  int init(const blocksstable::ObDatumRow &row, const int64_t read_snapshot_version);
+//  virtual int64_t size() const override;
+//  virtual int deep_copy(char *buf, const int64_t buf_len, ObIKVCacheValue *&value) const override;
+//  bool is_valid() const { return (nullptr != datums_ && 0 != column_cnt_) || (nullptr == datums_ && 0 == column_cnt_); }
+//  OB_INLINE ObStorageDatum *get_datums() const { return datums_; }
+//  OB_INLINE int64_t get_column_cnt() const { return column_cnt_; }
+//  OB_INLINE int64_t get_read_snapshot_version() const { return read_snapshot_version_; }
+//  ObDmlRowFlag get_flag() const { return flag_; }
+//  TO_STRING_KV(KP_(datums), K_(size), K_(column_cnt), K_(read_snapshot_version), K_(flag));
+//private:
+//  ObStorageDatum *datums_;
+//  int64_t size_;
+//  int32_t column_cnt_;
+//  int64_t read_snapshot_version_;
+//  ObDmlRowFlag flag_;
+};
+
+class ObDASCache : public common::ObKVCache<ObDASCacheKey, ObDASCacheValue> {
 public:
 
 private:
