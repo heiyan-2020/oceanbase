b7c63329fb5c5242da4a127de62ebdcb0063e752
heiyan <heiyan2020@gmail.com>
2023-08-28 20:06:33 +0800

    save: use sync remote

--- src/sql/das/ob_das_ref.cpp
+++ src/sql/das/ob_das_ref.cpp
@@ -232,7 +232,7 @@ bool ObDASRef::is_all_local_task() const
 int ObDASRef::execute_all_task()
 {
   int ret = OB_SUCCESS;
-  const bool async = GET_MIN_CLUSTER_VERSION() >= CLUSTER_VERSION_4_1_0_0;
+  const bool async = false;
   // move local aggregated tasks to last for better concurrency.
   if (OB_FAIL(move_local_tasks_to_last())) {
     LOG_WARN("failed to move local tasks to last.", K(ret));
