08cfafc104bb0f4a4d76c4d88d244e8cbb925137
黑岩 <heiyan2020@gmail.com>
2023-09-19 16:01:57 +0800

    save: fast path don't initialize cond lock

--- src/storage/tx/ob_trans_define_v4.cpp
+++ src/storage/tx/ob_trans_define_v4.cpp
@@ -1532,13 +1532,8 @@ void TxCtxStateHelper::restore_state()
 
 int ObInvalidateCtx::wait_until_succ() {
   int ret = OB_SUCCESS;
-  int retry_times = 100;
-  int32_t tmp_res = 1;
-  while (retry_times-- > 0 && (tmp_res = get_succ()) == 0) {}
 
-  TRANS_LOG(WARN, "das cache: spin finished", K(retry_times), K(tmp_res));
-
-  if (tmp_res == 1) {
+  if (get_succ() == 1) {
     return ret;
   }
 
@@ -1548,8 +1543,6 @@ int ObInvalidateCtx::wait_until_succ() {
       TRANS_LOG(TRACE, "das cache: wait failed");
     }
   }
-
-  TRANS_LOG(WARN, "das cache: wait succ");
 
   return ret;
 }
